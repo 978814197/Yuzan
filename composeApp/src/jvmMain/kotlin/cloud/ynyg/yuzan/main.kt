91a7ce6f99196d6edaa2e64e5940ba257955d232
屿你有关 <978814197@qq.com>
2025-08-10 12:44:57 +0800

    feat: 添加应用图标 以及 多语言应用名

--- composeApp/src/jvmMain/kotlin/cloud/ynyg/yuzan/main.kt
+++ composeApp/src/jvmMain/kotlin/cloud/ynyg/yuzan/main.kt
@@ -2,11 +2,17 @@ package cloud.ynyg.yuzan
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import yuzan.composeapp.generated.resources.Res
+import yuzan.composeapp.generated.resources.app_icon
+import yuzan.composeapp.generated.resources.app_name
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Yuzan",
+        title = stringResource(Res.string.app_name),
+        icon = painterResource(Res.drawable.app_icon)
     ) {
         App()
     }
